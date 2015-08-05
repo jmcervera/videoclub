e1cb96635db677fd2451f0810aeef6ef3237dc64
Juanma Cervera <juanmacervera@gmail.com>
2015-08-05 19:58:08 +0200

    Adds feature for updating movie info

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,13 +14,29 @@ class MoviesController < ApplicationController
       flash[:notice] = "Movie has been created."
       redirect_to @movie
     else
-      flash.now[:notice] = "Movie has not been created."
-      render :new
+      flash.now[:alert] = "Movie has not been created."
+      render "new"
     end
   end
 
   def show
     @movie = Movie.find(params[:id])
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    if @movie.update(movie_params)
+      flash[:notice] = "Movie has been updated."
+      redirect_to @movie
+    else
+      flash.now[:alert] = "Movie has not been updated."
+      render "edit"
+    end
+
   end
 
   private
---
+++ spec/features/editing_movies_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.feature "Users can edit existing movies", type: :feature do
+
+  before do
+    FactoryGirl.create(:movie, title: "Star Wars")
+    visit "/"
+    click_link "Star Wars"
+    click_link "Edit Movie"
+  end
+
+  scenario "with valid attributes" do
+    fill_in "Plot", with: "A long time ago in a galaxy far, far away..."
+    click_button "Update Movie"
+
+    expect(page).to have_content "Movie has been updated."
+    expect(page).to have_content "A long time ago in a galaxy far, far away..."
+  end
+
+  scenario "with invalid attributes" do
+    fill_in "Title", with: ""
+    click_button "Update Movie"
+
+    expect(page).to have_content "Movie has not been updated."
+
+  end
+end
