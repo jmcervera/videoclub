20c1788b7064ac9769786f36698053214232ef55
Juanma Cervera <juanmacervera@gmail.com>
2015-08-05 19:36:32 +0200

    Adds feature to view a list of the movies

---
+++ spec/features/viewing_movies_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.feature "Users can view movies", type: :feature do
+  scenario "with movie details" do
+    movie = FactoryGirl.create(:movie, title: 'Lord of the Rings')
+
+    visit "/"
+    click_link "Lord of the Rings"
+    expect(page.current_url).to eq movie_url(movie)
+  end
+end
