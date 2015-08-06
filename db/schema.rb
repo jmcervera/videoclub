20f1733b75ff4bf0208a024712d5e9f3b1ecd409
Juanma Cervera <juanmacervera@gmail.com>
2015-08-06 19:32:41 +0200

    Adds scaffold for users, this time not BDD

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150806141944) do
+ActiveRecord::Schema.define(version: 20150806171526) do
 
   create_table "episodes", force: :cascade do |t|
     t.integer  "number"
@@ -34,6 +34,12 @@ ActiveRecord::Schema.define(version: 20150806141944) do
   create_table "seasons", force: :cascade do |t|
     t.string   "title"
     t.text     "plot"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
